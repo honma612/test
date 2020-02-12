f4ca6a3bb7b7aa73fa558de37a6fb50c5bee88f0
honma612 <oshiri_hakase@yahoo.co.jp>
2020-02-12 16:26:17 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext('2d');
 int y = 0;
 
 function draw() {
-    ctx.fillRect(100,0,80,y++);
+    ctx.fillRect(50,50,80,y++);
   
     window.requestAnimationFrame(draw);
 }
